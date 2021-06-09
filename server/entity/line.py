65af88085567907066c5994c5b67097a5cbd6386
AlexanderSidorevich <alexandersidorevich@yandex.ru>
2021-06-09 16:42:01 +0300

    Change default constructor parameters

--- server/entity/line.py
+++ server/entity/line.py
@@ -6,7 +6,7 @@ from entity.serializable import Serializable
 class Line(Serializable):
     """ Line entity defined by: two points (p0, p1), length, unique id.
     """
-    def __init__(self, idx, length, p0, p1):
+    def __init__(self, idx, length=0, p0=None, p1=None):
         self.idx = idx
         self.length = length
         self.points = (p0, p1)
