e123c652c660c8a61e6307d05f1963529a50aa3d
AlexanderSidorevich <alexandersidorevich@yandex.ru>
2021-06-09 16:38:16 +0300

    Rename graph script

--- server/graph/graph.py
+++ server/graph/graph.py
@@ -1,21 +1,10 @@
 import heapq
+from pprint import pprint
 
 
 class Graph:
     def __init__(self, data):
         self.points = Graph.parse_data(data)
-
-    @staticmethod
-    def parse_data(data):
-        points = dict(map(lambda point: (point.get('idx'), list()), data.get('points', tuple())))
-        connections = map(lambda line: (line.get('points'), line.get('length')), data.get('lines', tuple()))
-
-        for connection in connections:
-            points_two, lenght = connection
-            point_from, point_to = points_two
-            points[point_from].append((lenght, point_to))
-            points[point_to].append((lenght, point_from))
-        return points
 
     def dijkstra(self, start_idx=None, end_idx=None):
         if start_idx is not None and start_idx not in self.points:
@@ -47,13 +36,69 @@ class Graph:
         if end_idx is None:
             return [[point_idx, weight, Graph.get_path(parents, point_idx)]
                     for point_idx, weight in weights.items()]
-        return [end_idx, weights[end_idx], Graph.get_path(parents, end_idx)]
+        return {'line_idx': end_idx, 'weight': weights[end_idx], 'path': Graph.get_path(parents, end_idx)}
+
+    @staticmethod
+    def parse_data(data):
+        points = dict(map(lambda point: (point.get('idx'), list()), data.get('points', tuple())))
+        connections = map(lambda line: (line.get('points'), line.get('length')), data.get('lines', tuple()))
+
+        for connection in connections:
+            points_two, lenght = connection
+            point_from, point_to = points_two
+            points[point_from].append((lenght, point_to))
+            points[point_to].append((lenght, point_from))
+        return points
 
     @staticmethod
     def get_path(parents, end_idx):
-        path = [end_idx]
+        path = [{'line_idx': end_idx, 'speed': 0}]
         current = end_idx
         while parents[current] is not None:
-            path.insert(0, parents[current])
+            parent = parents[current]
+            path.insert(0, {'line_idx': parent, 'speed': current - parent})
             current = parents[current]
         return path
+
+
+data = {
+    "idx": 1,
+    "lines": [
+        {
+            "idx": 193,
+            "length": 2,
+            "points": [
+                101,
+                102
+            ]
+        },
+        {
+            "idx": 194,
+            "length": 2,
+            "points": [
+                102,
+                103
+            ]
+        },
+
+    ],
+    "name": "map01",
+    "points": [
+        {
+            "idx": 101,
+            "post_idx": 13
+        },
+        {
+            "idx": 102,
+            "post_idx": None
+        },
+        {
+            "idx": 103,
+            "post_idx": None
+        },
+
+    ]
+}
+g = Graph(data)
+
+pprint(g.dijkstra(101, 103))
