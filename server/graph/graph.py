74f2320157dda9682c563836a51a176b0a164e30
AlexanderSidorevich <alexandersidorevich@yandex.ru>
2021-06-10 20:17:53 +0300

    Fix algorithm dijkstra

--- server/graph/graph.py
+++ server/graph/graph.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 
 class Graph:
-    def __init__(self, data):
+    def __init__(self, data, ):
         self.points = Graph.parse_data(data)
 
     def dijkstra(self, start_idx, end_idx):
@@ -23,7 +23,7 @@ class Graph:
             if point_idx in checked_points:
                 continue
             for connection in self.points[point_idx]:
-                point_to_weight, point_to_idx = connection
+                point_to_weight, point_to_idx, line_idx = connection
                 if point_to_idx not in checked_points and weight + point_to_weight < weights[point_to_idx]:
                     weights[point_to_idx] = weight + point_to_weight
                     parents[point_to_idx] = point_idx
@@ -31,26 +31,30 @@ class Graph:
 
             checked_points.add(point_idx)
 
-        return {'line_idx': end_idx, 'weight': weights[end_idx], 'path': Graph.get_path(parents, end_idx)}
+        return {'line_idx': end_idx, 'weight': weights[end_idx], 'path': self.get_path(parents, end_idx)}
+
+    def get_path(self, parents, end_idx):
+        path = []
+        current = end_idx
+        while parents[current] is not None:
+            parent = parents[current]
+            path.insert(0, {
+                'line_idx': next(filter(lambda connection: connection[1] == parent,
+                                        self.points[current]))[2],
+                'speed': 1 if current > parent else -1}
+                        )
+            current = parents[current]
+        return path
 
     @staticmethod
     def parse_data(data):
         points = dict(map(lambda point: (point.get('idx'), list()), data.get('points', tuple())))
-        connections = map(lambda line: (line.get('points'), line.get('length')), data.get('lines', tuple()))
+        connections = map(lambda line: (line.get('points'), line.get('length'), line.get('idx')),
+                          data.get('lines', tuple()))
 
         for connection in connections:
-            points_two, lenght = connection
-            point_from, point_to = points_two
-            points[point_from].append((lenght, point_to))
-            points[point_to].append((lenght, point_from))
+            two_points, length, line_idx = connection
+            from_point, to_point = two_points
+            points[from_point].append((length, to_point, line_idx))
+            points[to_point].append((length, from_point, line_idx))
         return points
-
-    @staticmethod
-    def get_path(parents, end_idx):
-        path = [{'line_idx': end_idx, 'speed': 0}]
-        current = end_idx
-        while parents[current] is not None:
-            parent = parents[current]
-            path.insert(0, {'line_idx': parent, 'speed': current - parent})
-            current = parents[current]
-        return path
